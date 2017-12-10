882a250140a402a1fc4bfb4f996f67462e72ad64
Doug Mahugh <dmahugh@microsoft.com>
2017-12-10 13:25:37 -0800

    document AUTHORITY_URL options

--- config.py
+++ config.py
@@ -1,23 +1,29 @@
-"""
-This file contains configuration settings for running the Python auth samples
-locally. In a production deployment, this information should be saved in a
-database or other secure storage mechanism.
+"""Configuration settings for running the Python auth samples locally.
+
+In a production deployment, this information should be saved in a database or
+other secure storage mechanism.
 """
 
-CLIENT_ID = 'PUT YOUR CLIENT ID HERE'
-CLIENT_SECRET = 'PUT YOUR CLIENT SECRET HERE'
+CLIENT_ID = 'ENTER_YOUR_CLIENT_ID'
+CLIENT_SECRET = 'ENTER_YOUR_SECRET'
 REDIRECT_URI = 'http://localhost:5000/login/authorized'
 
+# AUTHORITY_URL ending determines type of account that can be authenticated:
+# /organizations = organizational accounts only
+# /consumers = MSAs only (Microsoft Accounts - Live.com, Hotmail.com, etc.)
+# /common = allow both types of accounts
 AUTHORITY_URL = 'https://login.microsoftonline.com/common'
+
 AUTH_ENDPOINT = '/oauth2/v2.0/authorize'
 TOKEN_ENDPOINT = '/oauth2/v2.0/token'
 
 RESOURCE = 'https://graph.microsoft.com/'
 API_VERSION = 'v1.0'
-SCOPES = ['User.Read'] # add other scopes/permissions as needed
+SCOPES = ['User.Read'] # Add other scopes/permissions as needed.
+
 
 # This code can be removed after configuring CLIENT_ID and CLIENT_SECRET above.
-if ' ' in CLIENT_ID or ' ' in CLIENT_SECRET:
+if 'ENTER_YOUR' in CLIENT_ID or 'ENTER_YOUR' in CLIENT_SECRET:
     print('ERROR: config.py does not contain valid CLIENT_ID and CLIENT_SECRET')
     import sys
     sys.exit(1)
